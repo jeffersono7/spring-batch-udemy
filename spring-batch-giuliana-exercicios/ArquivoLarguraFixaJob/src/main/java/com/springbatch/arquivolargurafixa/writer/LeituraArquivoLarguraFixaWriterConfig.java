12649c1e7e4472c9f28b00ce029bc909d1944b75
Jefferson Farias <jefferson.farias7@gmail.com>
2020-09-08 22:13:02 -0300

    aulas

---
+++ spring-batch-giuliana-exercicios/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -0,0 +1,22 @@
+package com.springbatch.arquivolargurafixa.writer;
+
+import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LeituraArquivoLarguraFixaWriterConfig {
+    @Bean
+    public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
+		return items -> items.forEach(System.out::println);
+//        return items -> {
+//            for (Cliente cliente : items) {
+//                if (cliente.getNome().equals("Maria")) {
+//                    throw new Exception();
+//                }
+//                System.out.println(cliente);
+//            }
+//        };
+    }
+}
